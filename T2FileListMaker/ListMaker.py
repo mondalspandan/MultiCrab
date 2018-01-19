4634c302b46dff62e9677449fc9d386453200859
mondalspandan <mondalspandan@gmail.com>
2018-01-19 21:52:24 +0530

    Added exception for failed folders

--- T2FileListMaker/ListMaker.py
+++ T2FileListMaker/ListMaker.py
@@ -1,14 +1,15 @@
 import os
 os.system("mkdir -p Filelist")
 
-f=open("SkimmedTreesList.txt","r")
-filepref="SkimmedTrees_"
+f=open("Filelist_SkimmedTrees_MET_180115.txt","r")
+filepref="SkimmedTrees_MET_180115"
 
 pref="root://se01.indiacms.res.in:1094/"
 filecount=1
 lineminus1=""
 lineminus2=""
 fileopen=False
+failedfile=False
 log=open("log.txt","w")
 
 for line in f:
@@ -20,17 +21,20 @@ for line in f:
 	if fname.endswith(".root") and not fileopen:
 		folder=pref+lineminus2[:-2]+"/"
 #		print lineminus2
-#        log.write(lineminus2.split("/")[-3]+" "+filepref+str(filecount)+".txt\n")          # For making a list of CRAB job outputs
-		log.write(lineminus2.split("/")[-1][:-2]+" "+filepref+str(filecount)+".txt\n")      # For making a list of SkimmedTrees
-		out=open("Filelist/"+filepref+str(filecount)+".txt","w")		
-		out.write(folder+fname+"\n")
-		filecount+=1
+		if lineminus2.split("/")[-1].strip()=="failed:": failedfile=True
+		if not failedfile:
+#			log.write(lineminus2.split("/")[-3]+" "+filepref+str(filecount)+".txt\n")          # For making a list of CRAB job outputs
+			log.write(lineminus2.split("/")[-1][:-2]+" "+filepref+str(filecount)+".txt\n")      # For making a list of SkimmedTrees
+			out=open("Filelist/"+filepref+str(filecount)+".txt","w")		
+			out.write(folder+fname+"\n")
+			filecount+=1
 		fileopen=True
 	elif fname.endswith(".root"):
-		out.write(folder+fname+"\n")
+		if not failedfile: out.write(folder+fname+"\n")
 	elif fileopen:
-		out.close()
-		fileopen=False
+	    if not failedfile: out.close()
+	    fileopen=False
+	    failedfile=False
 	
 	lineminus2=lineminus1
 	lineminus1=line
